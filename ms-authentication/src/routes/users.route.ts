620fdfd3e28c30710d346146eb45c65f9108c72d
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-11-06 20:12:27 -0300

    Facilitando uso de statusCode no código

--- ms-authentication/src/routes/users.route.ts
+++ ms-authentication/src/routes/users.route.ts
@@ -1,17 +1,17 @@
 import { NextFunction, Request, Response, Router } from "express";
-
+import { StatusCodes } from 'http-status-codes'
 
 
 const usersRoute = Router()
 
 usersRoute.get('/users', ( req: Request, res: Response, next: NextFunction ) => {
     const users = [{ userName: 'Diogo' }]
-    res.status(200).send(users)
+    res.status(StatusCodes.OK).send(users)
 })
 
 usersRoute.get('/users/:uuid', ( req: Request<{ uuid: string }>, res: Response, next: NextFunction ) => {
     const uuid = req.params.uuid
-    res.status(200).send({ uuid })
+    res.status(StatusCodes.OK).send({ uuid })
 })
 
 export default usersRoute
