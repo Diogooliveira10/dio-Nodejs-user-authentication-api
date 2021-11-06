a45700475b4c18a6e6c85e37a42c1af80c5f1bbd
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-11-06 20:25:48 -0300

    Desenvolvendo e testando o método post

--- ms-authentication/src/routes/users.route.ts
+++ ms-authentication/src/routes/users.route.ts
@@ -14,4 +14,12 @@ usersRoute.get('/users/:uuid', ( req: Request<{ uuid: string }>, res: Response, 
     res.status(StatusCodes.OK).send({ uuid })
 })
 
+usersRoute.post('/users', ( req: Request, res: Response, next: NextFunction ) => {
+    const newUser = req.body
+
+    console.log(req.body)
+
+    res.status(StatusCodes.CREATED).send(newUser)
+})
+
 export default usersRoute
