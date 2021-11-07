cf43509b528b4463f5673321a65ac792a2c7132a
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-11-07 13:53:10 -0300

    Desenvolvendo o update e o delete de usuários

--- ms-authentication-pg/src/repositories/user.repository.ts
+++ ms-authentication-pg/src/repositories/user.repository.ts
@@ -36,12 +36,36 @@ class UserRepository {
             VALUES ($1, crypt($2, 'my_salt'))
             RETURNING uuid
         `
-        const values = [user.username, user.password];
+        const values = [user.username, user.password]
 
-        const { rows } = await db.query<{ uuid: string }>(script, values);
-        const [ newUser ] = rows;
+        const { rows } = await db.query<{ uuid: string }>(script, values)
+        const [ newUser ] = rows
 
-        return newUser.uuid;
+        return newUser.uuid
+    }
+
+    async update(user: User): Promise<void> {
+        const script = `
+            UPDATE application_user 
+            SET 
+                username = $1,
+                password = crypt($2, 'my_salt')
+            WHERE uuid = $3
+        `
+
+        const values = [user.username, user.password, user.uuid]
+        await db.query(script, values)
+    }
+
+    async remove(uuid: string): Promise<void> {
+        const cript = `
+            DELETE
+            FROM application_user
+            WHERE uuid = $1
+        `
+
+        const values = [ uuid ]
+        await db.query(cript, values)
     }
 }
 
