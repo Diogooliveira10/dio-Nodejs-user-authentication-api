b616c7f13fd247cec3caa58bdd1fea9ac969a9b3
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-11-07 14:15:52 -0300

    Realizando tratamentos de erros

--- ms-authentication-pg/src/routes/users.route.ts
+++ ms-authentication-pg/src/routes/users.route.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { StatusCodes } from 'http-status-codes'
+import { DatabaseError } from "pg-protocol";
 import userRepository from "../repositories/user.repository";
 
 const usersRoute = Router()
@@ -12,9 +13,17 @@ usersRoute.get('/users', async ( req: Request, res: Response, next: NextFunction
 
 // Buscar usuário específico
 usersRoute.get('/users/:uuid', async ( req: Request<{ uuid: string }>, res: Response, next: NextFunction ) => {
-    const uuid = req.params.uuid
-    const user = await userRepository.findById(uuid)
-    res.status(StatusCodes.OK).send(user)
+    try {
+        const uuid = req.params.uuid
+        const user = await userRepository.findById(uuid)
+        res.status(StatusCodes.OK).send(user)     
+    } catch (error) {
+        if (error instanceof DatabaseError) {
+            res.sendStatus(StatusCodes.BAD_REQUEST)
+        } else {
+            res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+        }
+    }
 })
 
 // Criar usuário
