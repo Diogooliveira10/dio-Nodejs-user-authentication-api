80365a99a042c0fd8d7eb9bc1330328c96789715
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-11-13 09:54:23 -0300

    Criando token com JWT

--- ms-authentication-jwt/src/routes/authorization.route.ts
+++ ms-authentication-jwt/src/routes/authorization.route.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response, Router } from 'express';
 import ForbiddenError from '../models/errors/forbidden.error.model';
 import userRepository from '../repositories/user.repository';
 import JWT from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
 
 const authorizationRoute = Router()
 
@@ -30,7 +31,18 @@ authorizationRoute.post('/token', async ( req: Request, res: Response, next: Nex
         }
 
         const user = await userRepository.findByUsernameAndPassword(username, password)
-        console.log(user)
+        
+        if (!user) {
+            throw new ForbiddenError('username or password is invalid!')
+        }
+
+        const jwtPayload = { username: user.username }
+        const jwtOptions = { subject: user?.uuid }
+        const secretKey = 'my_secret_key'
+
+        const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions)
+
+        res.status(StatusCodes.OK).json({ token: jwt })
 
     } catch (error) {
         next(error)
