ec4c487fad858245c61fa97b98dd41e9c82c63c5
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-11-13 09:45:02 -0300

    Instalação da biblioteca JWT para geração de tokens

--- ms-authentication-jwt/src/routes/authorization.route.ts
+++ ms-authentication-jwt/src/routes/authorization.route.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import ForbiddenError from '../models/errors/forbidden.error.model';
 import userRepository from '../repositories/user.repository';
+import JWT from 'jsonwebtoken';
 
 const authorizationRoute = Router()
 
@@ -38,3 +39,11 @@ authorizationRoute.post('/token', async ( req: Request, res: Response, next: Nex
 })
 
 export default authorizationRoute
+
+// "iss" O domínio de aplicação geradora de token
+// "sub" É o assunto do token, mas é muito utilizado para guarda o ID do usuário
+// "aud" Define quem pode usar o token
+// "exp" Data para expiração do token
+// "nbf" Define uma data para qual o token não pode ser aceito antes dela
+// "iat" Data de criação do token
+// "jti" O id do token
